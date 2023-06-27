e97e13b3923c9073508b8021d687de25897fa2ad
upload42 <55660242+upload42@users.noreply.github.com>
2023-06-27 19:44:23 -0300

    send_email.R

--- send_email.R
+++ send_email.R
@@ -1,11 +1,8 @@
 library(blastula)
 
-password <- Sys.getenv("Senha_teste01")
-
-create_smtp_creds_file(file="forecast",
+create_smtp_creds_file(file="forecast1",
                        user = "trabalho_series@outlook.com",
-                       provider = "outlook",
-                       password = password)
+                       provider = "outlook")
 
 my_email_object <- render_connect_email("Forecast.qmd")
 
@@ -13,5 +10,5 @@ smtp_send(my_email_object,
           from = "trabalho_series@outlook.com",
           to = "trabalho_series@outlook.com",
           subject = "Teste",
-          credentials = creds_file("forecast"))
+          credentials = creds_file("forecast1"))
 
